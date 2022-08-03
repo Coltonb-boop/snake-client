2d36dd2e406a1e315f5ab011ed8a71f9a96c9014
Coltonb-boop <coltonbitz@gmail.com>
2022-08-03 10:30:23 -0600

    pushing name to server and receiving server messages in client.js

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ const net = require('net');
 
 // establishes connection with game server
 const connect = () => {
+  console.log('Connecting...');
   const conn = net.createConnection({
     host: '172.31.40.113',
     port: 50541
@@ -10,6 +11,12 @@ const connect = () => {
   // interpret incoming data
   conn.setEncoding('utf8');
 
+  conn.on('connect', (message) => {
+    console.log('Successfully connected to the server');
+    conn.write('Name: CB');
+  });
+
+
   conn.on('data', (data) => {
     console.log('server says: ', data);
   });
@@ -17,7 +24,6 @@ const connect = () => {
   return conn;
 };
 
-console.log('Connecting...');
 
 module.exports = {
   connect
