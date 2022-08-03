f987bf46a79480643cd4f5bb977786419b3b004e
Coltonb-boop <coltonbitz@gmail.com>
2022-08-03 16:22:07 -0600

    cleaned up code and added constants.js

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 // establishes connection with game server
 const connect = () => {
   console.log('Connecting...');
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data
